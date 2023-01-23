bc8b3d4875c7cc21d266d31045e995ff4edf511b
MuhammadqodirProgrammer <komilovm831@gmail.com>
2023-01-23 12:27:45 +0500

    country data

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -1,18 +1,22 @@
-import './country.css'
-import  imgCountry from './Uzbekistan.png'
+import "./country.css";
+import imgCountry from "./Uzbekistan.png";
 console.log(imgCountry);
-function Country() {
+
+
+
+function Country(promps) {
+    console.log(promps,"Prompsssssss");
     return (
-       
-        <div className="list">
-   <img src={imgCountry} alt="country"  />
-            <h2 className="country">UZBEKISTAN</h2>
-            <p>Population:34232050</p>
-            <p>Capital:Tashkent</p>
-            <p>region:Asia</p>
-<button className='btn-more'>More...</button>
-        </div>
-    )
+      <div className="list">
+        <img src={promps.img} alt="country" />
+<div className="card-body">
+<h2 className="country">{promps.name} </h2>
+        <p>Capital: {promps.capital}</p>
+        <p>region:{promps.region} </p>
+        <button className="btn-more">More...</button>
+</div>
+      </div>
+    );
 }
 
 export default Country;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,13 @@ import './header.css'
 import mode from './moon-2304.svg'
 // import mode2 from './circle-half-stroke-solid.svg'
 function Header() {
+  const handleDarkMode =()=>{
+    document.body.classList.toggle("dark_mode")
+  }
     return(
         <header className="header">
         <h1>Country</h1>
-      <button className='mode'>
+      <button className='mode' onClick={handleDarkMode}>
       <img className='mode-img' src={mode} alt="mode" ></img>
 
       Dark-mode</button>
